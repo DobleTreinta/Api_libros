a4455523631d582c8920451de2a681995ceee7d8
DobleTreinta <anthonyovie15gmail.com>
2023-04-27 07:41:22 -0500

    agregando dos clases que seria clase libro y clase libro favorito

---
+++ src/main/java/pe/edu/upao/apilibros/models/Book.java
@@ -0,0 +1,31 @@
+package pe.edu.upao.apilibros.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="books")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="title",nullable = false)
+    private String title;
+    @Column(name="author",nullable = false)
+    private String author;
+    @Column(name="description",nullable = false)
+    private String description;
+    @Column(name="image_url",nullable = false)
+    private String imageUrl;
+    /*@Column(name="create_at")
+    private LocalDateTime createdAt;
+    @Column(name="update_at")
+    private LocalDateTime updatedAt;*/
+}
