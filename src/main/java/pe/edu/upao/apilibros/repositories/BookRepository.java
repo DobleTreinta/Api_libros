4d08d16b32756fc5d5f9dd979c4b72ba03d19ab4
DobleTreinta <anthonyovie15gmail.com>
2023-04-29 18:11:12 -0500

    modificando la clases bookRepository

--- src/main/java/pe/edu/upao/apilibros/repositories/BookRepository.java
+++ src/main/java/pe/edu/upao/apilibros/repositories/BookRepository.java
@@ -2,9 +2,14 @@ package pe.edu.upao.apilibros.repositories;
 
 import pe.edu.upao.apilibros.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BookRepository extends JpaRepository<Book,Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findAll();
+    Optional<Book> findById(Long id);
+    List<Book> findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(String title, String author);
+    Book save( Book book);
 }
